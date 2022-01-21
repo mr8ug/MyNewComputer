ff83ea1f951ca6c14b365ea71d99d0b0e2a23360
Carlos Campos <carloscamposprivated@gmail.com>
2022-01-21 15:18:45 -0600

    make it better

--- WebScraper/recolector.py
+++ WebScraper/recolector.py
@@ -73,17 +73,17 @@ class Recolector():
 
 
 
-#testLinks=['http://www.intelaf.com/precios_stock_detallado.aspx?codigo=CAM-NXT-SMW4U2','http://www.imeqmo.com/shop/product/bx8070110400-procesador-intel-core-i5-10400-2-9ghz-10th-gen-12373','http://www.macrosistemas.com/productos/proyectores/epson,-proyector-power-lite-e10-,-h975a,-hdmi,-3lcd,-3600-lumenes-detail','http://www.intelaf.com/precios_stock_detallado.aspx?codigo=CAM-NXT-SMW4U2']
-#testLinks=['http://www.imeqmo.com/shop/product/bx8070110100f-procesador-intel-core-i3-10100f-3-6ghz-10th-gen-13250?category=11',
-#            'http://www.imeqmo.com/shop/product/h410m-h-motherboard-gigabyte-h410m-h-socket-lga1200-10th-gen-2xddr4-micro-atx-venta-con-equipo-12522?category=10',
-#            'http://www.imeqmo.com/shop/product/sa400s37-960g-unidad-ssd-2-5-960gb-kingston-a400-500mbs-8735?category=144',
-#            'http://www.imeqmo.com/shop/product/st2000dm008-disco-duro-3-5-2tb-seagate-barracuda-7200rpm-10818?category=23',
-#            'http://www.imeqmo.com/shop/product/ct4g4dfs8266-memoria-ddr4-dimm-4gb-crucial-2666mhz-10805?category=12',
-#            'http://www.imeqmo.com/shop/product/31ve060-0004p-fuente-de-poder-600w-cougar-vte600-80-plus-bronce-10683?category=28',
-#            'http://www.imeqmo.com/shop/product/lcaz-120r-argb-enfriador-liquido-120mm-azza-blizzard-argb-13359?category=150',
-#            'http://www.imeqmo.com/shop/product/3mfcb120-0001-ventilador-cougar-vortex-fcb-rgb-120mm-15573?category=150',
-#            'http://www.imeqmo.com/shop/product/tuf-rtx3080ti-12g-gaming-tarjeta-de-video-12gb-gddr6x-asus-geforce-rtx-3080-ti-lhr-tuf-gaming-hdmi-dp-pcie-4-0-14853?category=19',
-#            'http://www.imeqmo.com/shop/product/106amt0003-03-case-gaming-cougar-panzer-evo-rgb-full-torre-vidrio-templado-e-atx-sin-fuente-9785?category=9'
+#testLinks=['https://www.intelaf.com/precios_stock_detallado.aspx?codigo=CAM-NXT-SMW4U2','https://www.imeqmo.com/shop/product/bx8070110400-procesador-intel-core-i5-10400-2-9ghz-10th-gen-12373','https://www.macrosistemas.com/productos/proyectores/epson,-proyector-power-lite-e10-,-h975a,-hdmi,-3lcd,-3600-lumenes-detail','https://www.intelaf.com/precios_stock_detallado.aspx?codigo=CAM-NXT-SMW4U2']
+#testLinks=['https://www.imeqmo.com/shop/product/bx8070110100f-procesador-intel-core-i3-10100f-3-6ghz-10th-gen-13250?category=11',
+#            'https://www.imeqmo.com/shop/product/h410m-h-motherboard-gigabyte-h410m-h-socket-lga1200-10th-gen-2xddr4-micro-atx-venta-con-equipo-12522?category=10',
+#            'https://www.imeqmo.com/shop/product/sa400s37-960g-unidad-ssd-2-5-960gb-kingston-a400-500mbs-8735?category=144',
+#            'https://www.imeqmo.com/shop/product/st2000dm008-disco-duro-3-5-2tb-seagate-barracuda-7200rpm-10818?category=23',
+#            'https://www.imeqmo.com/shop/product/ct4g4dfs8266-memoria-ddr4-dimm-4gb-crucial-2666mhz-10805?category=12',
+#            'https://www.imeqmo.com/shop/product/31ve060-0004p-fuente-de-poder-600w-cougar-vte600-80-plus-bronce-10683?category=28',
+#            'https://www.imeqmo.com/shop/product/lcaz-120r-argb-enfriador-liquido-120mm-azza-blizzard-argb-13359?category=150',
+#            'https://www.imeqmo.com/shop/product/3mfcb120-0001-ventilador-cougar-vortex-fcb-rgb-120mm-15573?category=150',
+#            'https://www.imeqmo.com/shop/product/tuf-rtx3080ti-12g-gaming-tarjeta-de-video-12gb-gddr6x-asus-geforce-rtx-3080-ti-lhr-tuf-gaming-hdmi-dp-pcie-4-0-14853?category=19',
+#            'https://www.imeqmo.com/shop/product/106amt0003-03-case-gaming-cougar-panzer-evo-rgb-full-torre-vidrio-templado-e-atx-sin-fuente-9785?category=9'
 #            ]
 #c = Recolector(testLinks)
 #data = c.getData()
